6121838e1920e78c25f8ab8e45ac2fbf75f7c822
gio-mikeladze <mikeladzegio2@gmail.com>
2025-07-19 19:16:08 +0400

    second commit

--- level 044/homework/homework(8).py
+++ level 044/homework/homework(8).py
@@ -1,3 +1,6 @@
-def a_list(list):
-    print(list.sort())
-a_list(["apple","bmw",5])
+def Reverse_the_list(list):
+    reversed_list=[]
+    for i in list:
+        reversed_list.insert(0,i)
+    print(reversed_list)
+Reverse_the_list([5,6,7,8])
