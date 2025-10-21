e50c5887b4ffac4e4f77047cf84257dce2d1db0a
gio-mikeladze <mikeladzegio2@gmail.com>
2025-10-21 18:53:20 +0400

    second commit

--- level 070/homework/homework.py
+++ level 070/homework/homework.py
@@ -1,1 +1,17 @@
-#ვაკეთებ
+#Complete the solution so that the function will break up camel casing, using a space between words.
+
+#Example
+#"camelCasing"  =>  "camel Casing"
+#"identifier"   =>  "identifier"
+#""             =>  ""
+def solution(s):
+    word=""
+    list=[]
+    for i in s:
+        if i!=i.upper():
+            word+=i
+        else:
+            list.append(word)
+            word=i
+    list.append(word)
+    return " ".join(list)
